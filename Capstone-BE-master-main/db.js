b85270fe8c00f661f1f0dd5e1c6d9d3ac2b647d9
Sakthivel ✨ <162392942+sakthiveloo7@users.noreply.github.com>
2025-06-14 08:02:27 +0530

    CLEAN EASE-BE

---
+++ Capstone-BE-master-main/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const MONGODB_URL = 'mongodb+srv://ajayr aja0903:0lKHH1YZNSSVKV6R@cluster0.7yitm3k.mongodb.net/';
+
+mongoose.connect(MONGODB_URL);
+
+const connection = mongoose.connection;
+
+connection.on('error', (error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
+
+connection.once('open', () => {
+  console.log('Connected to MongoDB successfully!');
+});
+
+module.exports =mongoose
